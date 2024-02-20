2dc88505d1c2e0bdff6e5ebae170b121e66f5ee3
AMOOT\j.shahidi <jamalshahidi20@gmail.com>
2024-02-20 16:38:56 +0330

    Seed Data in Villa table

--- Bulky/Bulky.Infrastructure/Data/ApplicationDbContext.cs
+++ Bulky/Bulky.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,18 +1,41 @@
 ﻿using Bulky.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Bulky.Infrastructure.Data
 {
-    public class ApplicationDbContext :DbContext
+    public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var villasForSeed = new List<Villa>() { new Villa()
+                                                   {
+                                                      Id=1,
+                                                      Name="Sky",
+                                                      Description="Full of option",
+                                                      ImageUrl="https://placehold.co/600*401",
+                                                      Occupancy=4,
+                                                      Price=200,
+                                                      Sqft=550
+                                                   },
+                                                   new Villa
+                                                   {
+                                                      Id=2,
+                                                      Name="Blue Sky",
+                                                      Description="Full of option",
+                                                      ImageUrl="https://placehold.co/600*401",
+                                                      Occupancy=4,
+                                                      Price=300,
+                                                      Sqft=550
+                                                   }
+
+            };
+            modelBuilder.Entity<Villa>().HasData(villasForSeed);
         }
 
         public DbSet<Villa> Villas { get; set; }
---
+++ Bulky/Bulky.Infrastructure/Migrations/20240220130746_SeedVillaToDb.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedVillaToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "CreatedDate", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, null, "Full of option", "https://placehold.co/600*401", "Sky", 4, 200.0, 550, null },
+                    { 2, null, "Full of option", "https://placehold.co/600*401", "Blue Sky", 4, 300.0, 550, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
